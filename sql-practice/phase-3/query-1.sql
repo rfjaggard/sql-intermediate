3ce71e00517a8d99ce7b47c76ee41b5923957101
Travis Wood <tww2013@gmail.com>
2024-09-05 17:17:33 -0500

    phase3

--- sql-practice/phase-3/query-1.sql
+++ sql-practice/phase-3/query-1.sql
@@ -1,6 +1,9 @@
 -- Find Hermione's cats
-SELECT cats.name, owners.first_name 
+
+SELECT cats.name, owners.first_name
 FROM cats 
+JOIN cat_owners 
+on cats.id = cat_owners.cat_id 
 JOIN owners 
-on cats.id = owners.id 
+on cat_owners.owner_id = owners.id 
 WHERE owners.first_name = "Hermoine";
