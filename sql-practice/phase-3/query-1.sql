91cfced2e7e9443c486672f7a656ecaff141da2a
Travis Wood <tww2013@gmail.com>
2024-09-05 17:59:33 -0500

    phase4complete

--- sql-practice/phase-3/query-1.sql
+++ sql-practice/phase-3/query-1.sql
@@ -2,6 +2,6 @@
 
 SELECT cats.name
 FROM cats 
-JOIN owners 
-on cats.id = owners.id
-WHERE owners.first_name = "Hermoine";
+JOIN cat_owners ON cats.id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.id
+WHERE owners.first_name = 'Hermione';
--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,2 +1,8 @@
 -- Find All the Toys for Hermione's cats
--- Your code here 
+
+SELECT DISTINCT toys.name
+FROM toys 
+JOIN cats ON toys.cat_id = cats.id
+JOIN cat_owners ON cats.id = cat_owners.cat_id
+JOIN owners ON cat_owners.owner_id = owners.id
+WHERE owners.first_name = 'Hermione';
